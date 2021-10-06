abab17649dda023e07c24522b2443adb9b88326c
Sylvester Abiah <malonekeller@gmail.com>
2021-10-06 18:04:50 +0000

    Commented the InsertionSort.java file

--- InsertionSort.java
+++ InsertionSort.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class InsertionSort {
 
     public static void main (String[] args) {
-        
+
     }
 
     public static void Insertion(int nums[], int numSize) {
@@ -13,11 +13,13 @@ public class InsertionSort {
 
         for (m = 1; m < numSize; ++m) {
             n = m;
+            // insert value at nums[m] into sorted part
             while (n > 0 && nums[n] < nums[n - 1]) {
+                // Swap values at nums[n] and nums[n - 1]
                 temp = nums[n];
                 nums[n] = nums[n -1];
                 nums[n - 1] = temp;
-                --n;
+                --n; // Decrease n by 1
             }
         }
     }
